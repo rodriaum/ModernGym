e50810d794b6f823c4007d362397839238950662
Cidenai <Cidenai@C3-PC4>
2023-11-03 15:22:28 +0000

    Adicionar arquivos de projeto.

---
+++ ModernGym/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ModernGym
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira seu nome: ");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Olá " + name + ", quantos minutos você passou no ginásio? ");
+            double minutes = Convert.ToDouble(Console.ReadLine());
+
+            double total = minutes * 0.05;
+
+            Console.WriteLine(name  + ", pague no balcão " + total + " euros.");
+        }
+    }
+}
